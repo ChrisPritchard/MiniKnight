902ad8be8303a747af97b9b3b132fe1affbb6795
Chris Pritchard <chris.pritchard@edpay.nz>
2018-07-12 13:22:34 +1200

    Added other textures

--- View.fs
+++ View.fs
@@ -5,6 +5,13 @@ open Model
 
 let assetsToLoad = [
     Texture { key = "background"; path = "./Content/Sprites/background.png" }
+    Texture { key = "knight"; path = "./Content/Sprites/knight.png" }
+    Texture { key = "orc"; path = "./Content/Sprites/orc.png" }
+    Texture { key = "stoneFloor"; path = "./Content/Sprites/stoneFloor.png" }
+    Texture { key = "goldCoin"; path = "./Content/Sprites/goldCoin.png" }
+    Texture { key = "spikes"; path = "./Content/Sprites/spikes.png" }
+    Texture { key = "portalArrive"; path = "./Content/Sprites/portalArrive.png" }
+    Texture { key = "portalDepart"; path = "./Content/Sprites/portalDepart.png" }
 ]
 
 let screenWidth, screenHeight = 800, 600
