45ddee4936a2e8fd4ddd1b2e3fee49fb66ec911f
Chris Pritchard <chris.pritchard@edpay.nz>
2018-07-13 09:37:50 +1200

    implemented change direction used a mutable last command time prop

--- Controller.fs
+++ Controller.fs
@@ -1,8 +1,33 @@
 module Controller
 
+open GameCore
 open Model
+open Microsoft.Xna.Framework.Input
 
-let advanceGame runState =
+let timeBetweenCommands = 100.
+let mutable lastCommandTime = 0.
+
+let checkForDirectionChange runState current =
+    if runState.elapsed - lastCommandTime < timeBetweenCommands then current
+    else if runState.WasJustPressed Keys.Left && current <> Left then
+        lastCommandTime <- runState.elapsed
+        Left
+    else if runState.WasJustPressed Keys.Right && current <> Right then
+        lastCommandTime <- runState.elapsed
+        Right
+    else
+        current
+
+let handlePlayingState runState state =
+    let knightDir = checkForDirectionChange runState state.knight.direction
+
+    let newState = { state with knight = { state.knight with direction = knightDir } }
+    Some (Playing newState)
+
+let advanceGame (runState : RunState) =
     function
     | None -> Some startWorld
-    | Some world -> Some world
+    | Some world -> 
+        match world with
+        | Playing state -> handlePlayingState runState state 
+        | _ -> Some world
