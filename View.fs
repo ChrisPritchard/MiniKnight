45ae464289f7e4eb41089cbc533272d28032fb1f
Christopher <chrispritchard.nz@gmail.com>
2018-07-13 08:12:52 +1200

    started work on frame selector for knight

--- View.fs
+++ View.fs
@@ -17,10 +17,18 @@ let assetsToLoad = [
 let screenWidth, screenHeight = 800, 600
 let resolution = Windowed (screenWidth, screenHeight)
 
-let getPlayingView _ _ = 
+let getKnightFrame (knight : Knight) elapsed = 
+    let byDir leftFrame rightFrame = if knight.direction = Left then leftFrame else rightFrame
+    match knight.state with
+    | Standing -> byDir "standleft1" "standright1"
+    | Dead -> byDir "deadleft2" "deadright2"
+    | _ -> "standright1"
+
+let getPlayingView (runState : RunState) (state : PlayingState) =
+    let elapsed = runState.elapsed
     [
         Image ("background", (0,0,screenWidth,screenHeight), None)
-        MappedImage ("knight", "standright2", (screenWidth/2, screenHeight/2, 40, 40))
+        MappedImage ("knight", getKnightFrame state.knight elapsed, (screenWidth/2, screenHeight/2, 40, 40))
     ]
 
 let getView runState model =
