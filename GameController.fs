ef701d459b82ea0497ad54fda3dce5a712249880
Christopher <chrispritchard.nz@gmail.com>
2018-07-24 19:21:49 +1200

    cleanup of game controller match conditions

--- GameController.fs
+++ GameController.fs
@@ -12,21 +12,24 @@ let handlePlayingState runState worldState =
 
 let firstLevel = MapLoader.getLevel 1
 
-let advanceGame (runState : RunState) =
+let hasReset (runState : RunState) worldState = 
+    worldState.knight.state = Dead && runState.WasJustPressed Keys.R 
+
+let hasWarpedOut (runState : RunState) worldState = 
+    match worldState.knight.state with
+    | WarpingOut t when runState.elapsed - t > warpTime -> true 
+    | _ -> false
+
+let advanceGame runState =
     function
     | None -> 
         getLevelModel firstLevel runState.elapsed |> Some 
     | _ when runState.WasJustPressed Keys.Escape -> None
     | Some model -> 
         match model with
-        | Playing worldState when 
-                worldState.knight.state = Dead && 
-                runState.WasJustPressed Keys.R -> 
+        | Playing worldState when hasReset runState worldState -> 
             getLevelModel firstLevel runState.elapsed |> Some
-        | Playing worldState when 
-            (match worldState.knight.state with
-            | WarpingOut t when runState.elapsed - t > warpTime -> true 
-            | _ -> false) -> 
+        | Playing worldState when hasWarpedOut runState worldState -> 
             getLevelModel firstLevel runState.elapsed |> Some
         | Playing worldState -> 
             handlePlayingState runState worldState
