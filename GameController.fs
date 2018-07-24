e9f73aeddeab6639e67def4eac3fa84681cb5f75
Christopher <chrispritchard.nz@gmail.com>
2018-07-24 19:13:03 +1200

    Warping out now resets level

--- GameController.fs
+++ GameController.fs
@@ -2,6 +2,7 @@ module GameController
 
 open GameCore
 open Model
+open View
 open Microsoft.Xna.Framework.Input
 
 let handlePlayingState runState worldState =
@@ -18,6 +19,11 @@ let advanceGame (runState : RunState) =
         | Playing worldState when 
                 worldState.knight.state = Dead && 
                 runState.WasJustPressed Keys.R -> 
+            MapLoader.getLevel 1 |> getLevelModel |> Some
+        | Playing worldState when 
+            (match worldState.knight.state with
+            | WarpingOut t when runState.elapsed - t > warpTime -> true 
+            | _ -> false) -> 
             MapLoader.getLevel 1 |> getLevelModel |> Some
         | Playing worldState -> 
             handlePlayingState runState worldState
