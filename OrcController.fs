35417a340dbb2f85b452537e9408b959b36d8158
Christopher <chrispritchard.nz@gmail.com>
2018-07-25 18:21:27 +1200

    implemented bouncing off friends

--- OrcController.fs
+++ OrcController.fs
@@ -1,7 +1,6 @@
 module OrcController
 
 open Model
-open View
 
 let walkSpeed = 0.05
 
@@ -10,13 +9,24 @@ let checkForFloor (nx, y) direction blocks =
     let testx = if direction = Left then floor nx |> int else ceil nx |> int
     blocks |> List.exists (fun (bx, by, _) -> (bx, by) = (testx, testy))
 
+let checkForFriend (nx, y) direction = 
+    List.exists (fun (ox, oy) -> 
+        oy = y &&
+        (match direction with
+        | Left -> ox > nx - 1. && ox < nx
+        | Right -> nx < ox && nx + 1. > ox))
+
 let processOrc runState worldState (orc : Orc) =
     let x, y = orc.position
     let nx = if orc.direction = Left then x - walkSpeed else x + walkSpeed
-    let isEdge = checkForFloor (nx, y) orc.direction worldState.blocks |> not
 
-    let direction = if not isEdge then orc.direction else if orc.direction = Left then Right else Left
-    let position = if not isEdge then (nx, y) else orc.position
+    let otherOrcs = worldState.orcs |> List.except [orc] |> List.map (fun o -> o.position)
+    let shouldTurn = 
+        checkForFloor (nx, y) orc.direction worldState.blocks |> not
+        || checkForFriend (nx, y) orc.direction otherOrcs
+
+    let direction = if not shouldTurn then orc.direction else if orc.direction = Left then Right else Left
+    let position = if not shouldTurn then (nx, y) else orc.position
     
     { orc  with 
         direction = direction
