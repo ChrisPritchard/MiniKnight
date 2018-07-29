d5fd945b52519770e4ee501e7aff50d8f126a7db
Chris Pritchard <chrispritchard.nz@gmail.com>
2018-07-29 16:55:26 +1200

    moved maploader code into game controller

--- GameController.fs
+++ GameController.fs
@@ -4,6 +4,24 @@ open GameCore
 open Model
 open View
 open Microsoft.Xna.Framework.Input
+open System.Drawing
+
+let mapKey = [
+    (Color.FromArgb(128, 128, 128), Block)
+    (Color.FromArgb(255, 0, 0), Spikes)
+    (Color.FromArgb(38, 127, 0), EntryPortal)
+    (Color.FromArgb(0, 38, 255), ExitPortal)
+    (Color.FromArgb(255, 216, 0), Coin)
+    (Color.FromArgb(0, 0, 0), Orc)
+]
+
+let getLevel num = 
+    use bitmap = Bitmap.FromFile <| sprintf "./Content/Maps/map%i.bmp" num :?> Bitmap
+    [0..bitmap.Height-1] |> Seq.collect (fun y -> [0..bitmap.Width-1] |> Seq.map (fun x ->
+        let color = bitmap.GetPixel (x, y)
+        match Seq.tryFind (fun (c,_) -> c = color) mapKey with
+        | Some t -> Some (x, y, t |> snd)
+        | _ -> None)) |> Seq.choose id |> Seq.toList
 
 let handlePlayingState runState worldState =
     worldState
@@ -12,7 +30,7 @@ let handlePlayingState runState worldState =
     |> Playing |> Some
 
 let maxLevel = 5
-let levels = [1..maxLevel] |> List.map (fun i -> (i, MapLoader.getLevel i)) |> Map.ofList
+let levels = [1..maxLevel] |> List.map (fun i -> (i, getLevel i)) |> Map.ofList
 
 let hasReset (runState : RunState) worldState = 
     worldState.knight.state = Dead && runState.WasJustPressed Keys.R 
